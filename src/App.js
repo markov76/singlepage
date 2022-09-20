fd17f34b31d382468f64b03b60707ae28dc9e193
markov76 <101133846+markov76@users.noreply.github.com>
2022-09-20 11:50:06 +0300

    lisatty footer ym

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 import React from 'react';
+import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom'
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
   return (
- <div><h1>moi</h1></div>
+    <>
+      <Navbar />
+      <div className='container'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes> 
+      </div>
+    </>
   );
 }
 
